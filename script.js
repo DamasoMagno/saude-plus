ed3c3a63d1fe5d954460a2928d3cc9124fcb6161
DamasoMagno <limamdamaso@gmail.com>
2024-06-16 16:55:23 -0300

    feat: Replaced project to Saude+

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const header = document.querySelector("header");
+const button = document.querySelector("button")
 
 window.addEventListener("scroll", () => {
   if (this.scrollY >= 10) {
-    header.classList.add("headerActive");
+    header.classList.add("floatHeader");
   } else {
-    header.classList.remove("headerActive");
+    header.classList.remove("floatHeader");
   }
 });
+
+button.addEventListener("click", () => {
+  header.classList.toggle("mobile_menu")
+})
